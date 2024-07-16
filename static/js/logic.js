f4476746de2206a187a59f77618572cd5b5bd4ed
Ryan Eikanger <reikanger@gmail.com>
2024-07-16 15:02:36 -0500

    added initial maps objects

---
+++ static/js/logic.js
@@ -0,0 +1,20 @@
+// set URL for USGS earthquake data
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// request data from URL
+d3.json(url).then(function (data) {
+	// create base layer
+	let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	});
+
+	let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+		attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+	});
+
+	// Create a baseMaps object
+	let baseMaps = {
+		"Street Map": street,
+		"Topographic Map": topo
+	};
+});
