1b8d0556503e23f2d31da495fd69d05cace0c31a
Ryan Eikanger <reikanger@gmail.com>
2024-07-16 15:20:47 -0500

    first map complete

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,6 +3,10 @@ let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.ge
 
 // request data from URL
 d3.json(url).then(function (data) {
+	// print to console for debugging
+	console.log(data);
+	console.log(data.features);
+
 	// create base layer
 	let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -18,12 +22,30 @@ d3.json(url).then(function (data) {
 		"Topographic Map": topo
 	};
 
+	// loop data object for 'data.features' content
+	let features_array = [];
+	for (let i = 0; i < data.features.length; i++) {
+		feature = L.geoJSON(data.features[i]).bindPopup(`<h1>${data.features[i].properties.place}</h1><h3>Magnitude ${data.features[i].properties.mag}</h3>`);
+		features_array.push(feature);
+	};
+
+	let features_group = L.layerGroup(features_array);
+
+	// set Earthquakes feature to map
+	let overlayMaps = {
+		Earthquakes: features_group
+	};
+
 	// add leaflet map to map div
 	let myMap = L.map("map", {
-		layers: [street]
+		center: [
+			40.00, -115.00
+		],
+		zoom: 6,
+		layers: [street, features_group]
 	});
 
-	L.control.layers(baseMaps, {
+	L.control.layers(baseMaps, overlayMaps, {
 		collapsed: false
 	}).addTo(myMap);
 });
